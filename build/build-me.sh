34a9db932c94b0d0c55b55168e272fd9c7829ac0
Geoff R. McLane <ubuntu@geoffair.info>
2015-01-11 18:21:16 +0100

    add finding installed simgear

--- build/build-me.sh
+++ build/build-me.sh
@@ -3,6 +3,9 @@
 BN=`basename $0`
 
 BLDLOG="bldlog-1.txt"
+TMPSG="/media/Disk2/FG/fg21/install/simgear"
+TMPTIME=`date +%H:%M:%S`
+TMPDATE=`date +%Y/%m/%d`
 
 if [ -f "$BLDLOG" ]; then
 	rm -f $BLDLOG
@@ -14,6 +17,16 @@ fi
 ##############################################
 TMPOPTS="-DCMAKE_INSTALL_PREFIX=$HOME"
 #############################################
+### Add to DEBUG SimGear finding
+### TMPOPTS="$TMPOPTS -DSG_DBG_FINDING:BOOL=TRUE"
+
+echo "$BN: Build $TMPDATE $TMPTIME" >> $BLDLOG
+
+if [ -d "$TMPSG" ]; then
+    TMPOPTS="$TMPOPTS -DCMAKE_PREFIX_PATH:PATH=$TMPSG"
+    export SIMGEAR_DIR=$TMPSG
+    echo "$BN: exported SIMGEAR_DIR=$TMPSG" >> $BLDLOG
+fi
 
 echo "$BN: Doing: 'cmake .. $TMPOPTS' to $BLDLOG"
 cmake .. $TMPOPTS >> $BLDLOG 2>&1
